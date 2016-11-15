72cfaf96123f1ab0621095aaccf6c1803cbce5bb
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-11-15 05:14:11 -0500

    Actualiza

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,49 @@
 # encoding: UTF-8
 class Ability  < Cor1440Gen::Ability
 
+  # Ver documentacion de este metodo en app/models/ability de sip
+  def initialize(usuario = nil)
+    # Sin autenticación puede consultarse información geográfica 
+    can :read, [Sip::Pais, Sip::Departamento, Sip::Municipio, Sip::Clase]
+    if !usuario || usuario.fechadeshabilitacion
+      return
+    end
+    can :contar, Sip::Ubicacion
+    can :buscar, Sip::Ubicacion
+    can :lista, Sip::Ubicacion
+    can :descarga_anexo, Sip::Anexo
+    can :nuevo, Cor1440Gen::Actividad
+    can :nuevo, Sip::Ubicacion
+    if !usuario.nil? && !usuario.rol.nil? then
+      case usuario.rol 
+      when Ability::ROLSISTACT
+        can :read, Cor1440Gen::Informe
+        can :read, Cor1440Gen::Actividad
+        can :new, Cor1440Gen::Actividad
+        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
+          oficina: { id: usuario.oficina_id}
+      when Ability::ROLCOOR
+        can :manage, Cor1440Gen::Actividad
+        can :manage, Cor1440Gen::Informe
+        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
+          oficina: { id: usuario.oficina_id}
+        can :new, Usuario
+        can [:read, :manage], Usuario, oficina: { id: usuario.oficina_id}
+      when Ability::ROLINV
+        cannot :buscar, Sip::Actividad
+        can :read, Sip::Actividad
+      when Ability::ROLADMIN, Ability::ROLDIR
+        can :manage, Cor1440Gen::Actividad
+        can :manage, Cor1440Gen::Informe
+        can :manage, Usuario
+        can :manage, :tablasbasicas
+        tablasbasicas.each do |t|
+          c = Ability.tb_clase(t)
+          can :manage, c
+        end
+      end
+    end
+  end
+
 end
 
