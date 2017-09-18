8ed362b826d86876e946c33ba3926df08e2fc0c5
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-18 17:01:10 -0500

    usuario sobre Sip::modelo

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+require 'cor1440_gen/concerns/controllers/usuarios_controller'
+
+class UsuariosController < Sip::ModelosController
+  include Cor1440Gen::Concerns::Controllers::UsuariosController
+end
+
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,10 +1,7 @@
 # encoding: UTF-8
 
-require 'sip/concerns/models/usuario'
 require 'cor1440_gen/concerns/models/usuario'
 
 class Usuario < ActiveRecord::Base 
-  include Sip::Concerns::Models::Usuario
   include Cor1440Gen::Concerns::Models::Usuario
-
 end
