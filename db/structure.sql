74e00ffd4fd2d7f6e3fff99e70ff24d6e229b650
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-04-12 21:06:53 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -3204,7 +3204,7 @@ ALTER TABLE ONLY usuario
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO public, pg_catalog;
+SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20131128151014'),
