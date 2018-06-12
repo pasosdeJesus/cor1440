17072c83351e06e3de329122dd2be3f6f80aae1e
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-06-12 03:55:52 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -519,7 +519,8 @@ CREATE TABLE cor1440_gen_campoact (
     id bigint NOT NULL,
     actividadtipo_id integer,
     nombrecampo character varying(128),
-    ayudauso character varying(1024)
+    ayudauso character varying(1024),
+    tipo integer DEFAULT 1
 );
 
 
@@ -550,7 +551,8 @@ CREATE TABLE cor1440_gen_campotind (
     id bigint NOT NULL,
     tipoindicador_id integer NOT NULL,
     nombrecampo character varying(128) NOT NULL,
-    ayudauso character varying(1024)
+    ayudauso character varying(1024),
+    tipo integer DEFAULT 1
 );
 
 
@@ -3419,6 +3421,9 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180320230847'),
 ('20180427194732'),
 ('20180509111948'),
-('20180519102415');
+('20180519102415'),
+('20180611222635'),
+('20180612024009'),
+('20180612030340');
 
 
