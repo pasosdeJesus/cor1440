b214e154b7cf61ac653728f071c8d4d750eb188a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-04-01 19:51:15 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -4408,14 +4408,6 @@ ALTER TABLE ONLY public.sip_ubicacion
 
 
 --
--- Name: sip_trelacion trelacion_inverso_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.sip_trelacion
-    ADD CONSTRAINT trelacion_inverso_fkey FOREIGN KEY (inverso) REFERENCES public.sip_trelacion(id);
-
-
---
 -- Name: sip_ubicacion ubicacion_id_pais_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -4650,6 +4642,8 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190206005635'),
 ('20190208103518'),
 ('20190322102311'),
-('20190326150948');
+('20190326150948'),
+('20190331111015'),
+('20190401175521');
 
 
