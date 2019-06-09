4493924d9e7bfc0f452a84493f0806e2fb4574af
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-06-09 11:12:01 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,56 +1,71 @@
 #!/bin/sh
-# Hace pruebas, pruebas de regresión, envia a github y sube a heroku
+# Revisa errore comunes, ejecuta pruebas de regresión y del sistema y envia a github 
 
 if (test -f ".env") then {
 	. ./.env
 } fi;
-grep "^ *gem *.sip.*, *path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sip cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem *.cor1440_gen.*, *path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un cor1440_gen cableado al sistema de archivos"
-	exit 1;
-} fi;
+
+function cableado {
+	for n in $*; do 
+		echo "Revisando $n"
+		grep "^ *gem *.${n}.*, *path:" Gemfile > /dev/null 2> /dev/null
+		if (test "$?" = "0") then {
+			echo "Gemfile incluye un ${n} cableado al sistema de archivos"
+			exit 1;
+		} fi;
+	done
+}
+
+d=`grep "gem.*pasosdeJesus" Gemfile | sed -e "s/gem ['\"]//g;s/['\"].*//g"`
+cableado $d
+
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
-#grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
-#if (test "$?" = "0") then {
-#	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
-#	exit 1;
-#} fi;
+grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
+if (test "$?" = "0") then {
+	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
+	exit 1;
+} fi;
 
 if (test "$SINAC" != "1") then {
-  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
+	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+	CXX=c++ yarn upgrade
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
 } fi;
 if (test "$SININS" != "1") then {
 	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
 } fi;
 if (test "$SINMIG" != "1") then {
-	(bundle exec rake db:migrate sip:indices db:structure:dump)
+	(bin/rails db:migrate sip:indices db:structure:dump)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices
+RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
-bundle exec rails test
+CONFIG_HOSTS="127.0.0.1" bundle exec rails test:system
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas";
+	echo "No pasaron pruebas al sistema";
 	exit 1;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:structure:dump
+RAILS_ENV=test bin/rails db:structure:dump
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
@@ -63,7 +78,4 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
-if (test "$CONH" == "1") then {
-	git push heroku master
-	heroku run rake db:migrate
-} fi;
+
