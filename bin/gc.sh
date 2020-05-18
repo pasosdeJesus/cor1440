ff66ef6fbcb46a40df3871e79f3aaaa16eab1462
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-05-18 03:42:41 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -5,19 +5,11 @@ if (test -f ".env") then {
 	. ./.env
 } fi;
 
-function cableado {
-	for n in $*; do 
-		echo "Revisando $n"
-		grep "^ *gem *.${n}.*, *path:" Gemfile > /dev/null 2> /dev/null
-		if (test "$?" = "0") then {
-			echo "Gemfile incluye un ${n} cableado al sistema de archivos"
-			exit 1;
-		} fi;
-	done
-}
-
-d=`grep "gem.*pasosdeJesus" Gemfile | sed -e "s/.*gem ['\"]//g;s/['\"].*//g"`
-cableado $d
+s=`grep -B 1 "^ *path" Gemfile 2> /dev/null`
+if (test "$?" = "0") then {
+  echo "Gemfile incluye gema cableada al sistema de archivos ($s)"
+  exit 1;
+} fi;
 
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
@@ -63,11 +55,24 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
-CONFIG_HOSTS="127.0.0.1" bin/rails test:system
+CONFIG_HOSTS=www.example.com bin/rails test
+if (test "$?" != "0") then {
+	echo "No pasaron pruebas";
+	exit 1;
+} fi;
+
+CONFIG_HOSTS="www.example.com" bin/rails test:system
 if (test "$?" != "0") then {
 	echo "No pasaron pruebas al sistema";
 	exit 1;
 } fi;
+
+
+#RAILS_ENV=test CONFIG_HOSTS=127.0.0.1 bin/rails test:system
+#if (test "$?" != "0") then {
+#	echo "No pasaron pruebas de sistema";
+#	exit 1;
+#} fi;
 
 RAILS_ENV=test bin/rails db:structure:dump
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
@@ -82,4 +87,9 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
+
+if (test "$CONH" != "") then {
+	git push heroku master
+	heroku run rake db:migrate sip:indices
+} fi;
 
