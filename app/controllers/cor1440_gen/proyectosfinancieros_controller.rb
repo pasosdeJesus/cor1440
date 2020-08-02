ddda1428c9847b0ed705f2d636252df66ad12af5
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-08-02 15:00:10 -0500

    Posible generar listado de proyetos financieros en nueva plantilla y quitamos area

---
+++ app/controllers/cor1440_gen/proyectosfinancieros_controller.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+require_dependency "cor1440_gen/concerns/controllers/proyectosfinancieros_controller"
+
+module Cor1440Gen
+  class ProyectosfinancierosController < Heb412Gen::ModelosController
+    include Cor1440Gen::Concerns::Controllers::ProyectosfinancierosController
+
+    load_and_authorize_resource  class: Cor1440Gen::Proyectofinanciero,
+      only: [:new, :create, :destroy, :edit, :update, :index, :show,
+             :objetivospf]
+
+    def atributos_index
+      [ 
+        :id, 
+        :nombre 
+      ] +
+      [ :financiador_ids =>  [] ] +
+      [ 
+        :fechainicio_localizada,
+        :fechacierre_localizada,
+        :responsable,
+        :proyectofinanciero_usuario,
+      ] +
+      [ :compromisos, 
+        :monto, 
+        :observaciones, 
+        :objetivopf,
+        :indicadorobjetivo,
+        :resultadopf,
+        :indicadorpf,
+        :actividadpf
+      ]
+    end
+
+
+    def atributos_show
+      atributos_index - [
+        :objetivopf,
+        :indicadorobjetivo,
+        :resultadopf,
+        :indicadorpf,
+        :actividadpf
+      ] + [ 
+        :marcologico,
+        :caracterizacion, 
+        :beneficiario,
+        :plantillahcm,
+        :anexo_proyectofinanciero
+      ]
+    end
+
+
+  end 
+end
