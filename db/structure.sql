083cb3bf96a6eaf2c290eeb124ea07491c2268c5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-12-05 07:58:59 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -1260,7 +1260,8 @@ CREATE TABLE public.cor1440_gen_proyectofinanciero (
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
     compromisos character varying(5000),
-    monto integer
+    monto integer,
+    sectorapc_id integer
 );
 
 
@@ -1381,6 +1382,40 @@ CREATE SEQUENCE public.cor1440_gen_resultadopf_id_seq
 --
 
 ALTER SEQUENCE public.cor1440_gen_resultadopf_id_seq OWNED BY public.cor1440_gen_resultadopf.id;
+
+
+--
+-- Name: cor1440_gen_sectorapc; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.cor1440_gen_sectorapc (
+    id bigint NOT NULL,
+    nombre character varying(500) NOT NULL,
+    observaciones character varying(5000),
+    fechacreacion date NOT NULL,
+    fechadeshabilitacion date,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: cor1440_gen_sectorapc_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.cor1440_gen_sectorapc_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: cor1440_gen_sectorapc_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.cor1440_gen_sectorapc_id_seq OWNED BY public.cor1440_gen_sectorapc.id;
 
 
 --
@@ -3331,6 +3366,13 @@ ALTER TABLE ONLY public.cor1440_gen_resultadopf ALTER COLUMN id SET DEFAULT next
 
 
 --
+-- Name: cor1440_gen_sectorapc id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cor1440_gen_sectorapc ALTER COLUMN id SET DEFAULT nextval('public.cor1440_gen_sectorapc_id_seq'::regclass);
+
+
+--
 -- Name: cor1440_gen_tipoindicador id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -3851,6 +3893,14 @@ ALTER TABLE ONLY public.cor1440_gen_proyectofinanciero_usuario
 
 ALTER TABLE ONLY public.cor1440_gen_resultadopf
     ADD CONSTRAINT cor1440_gen_resultadopf_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: cor1440_gen_sectorapc cor1440_gen_sectorapc_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cor1440_gen_sectorapc
+    ADD CONSTRAINT cor1440_gen_sectorapc_pkey PRIMARY KEY (id);
 
 
 --
@@ -4785,6 +4835,14 @@ ALTER TABLE ONLY public.cor1440_gen_actividad_rangoedadac
 
 ALTER TABLE ONLY public.cor1440_gen_valorcampoact
     ADD CONSTRAINT fk_rails_3060a94455 FOREIGN KEY (campoact_id) REFERENCES public.cor1440_gen_campoact(id);
+
+
+--
+-- Name: cor1440_gen_proyectofinanciero fk_rails_3792591d9e; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cor1440_gen_proyectofinanciero
+    ADD CONSTRAINT fk_rails_3792591d9e FOREIGN KEY (sectorapc_id) REFERENCES public.cor1440_gen_sectorapc(id);
 
 
 --
@@ -5903,6 +5961,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200921123831'),
 ('20201009004421'),
 ('20201119125643'),
-('20201121162913');
+('20201121162913'),
+('20201205041350');
 
 
