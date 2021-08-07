8b24ae6e393b03a4efc99eb4c71b279fc63fcddc
Nombre Usuario <correo@usuario.net>
2021-08-07 08:14:33 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -4,6 +4,7 @@ SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
+SET default_toast_compression = 'pglz';
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
@@ -6243,6 +6244,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210614120835'),
 ('20210614212220'),
 ('20210616003251'),
-('20210619191706');
+('20210619191706'),
+('20210728214424');
 
 
