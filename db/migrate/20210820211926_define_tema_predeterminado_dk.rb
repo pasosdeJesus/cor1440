64bfc6f8939c2bd2e7cc1efcb83b5394ae6e4e48
Alejandro Cruz <alejocruzzz@gmail.com>
2021-08-20 17:25:31 -0500

    tema predeterminado

---
+++ db/migrate/20210820211926_define_tema_predeterminado_dk.rb
@@ -0,0 +1,58 @@
+class DefineTemaPredeterminadoDk < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      UPDATE sip_tema SET
+        nombre='PREDETERMINADO DASHBOARDKIT',
+        fondo = '#ffffff',
+        color_fuente = '#293240',
+        nav_ini = '#6450b9',
+        nav_fin = '#6450b9',
+        nav_fuente = '#ffffff',
+        fondo_lista = '#e3e1fc',
+        color_flota_subitem_fuente = '#7267EF',
+        color_flota_subitem_fondo = '#e3e1fc',
+        btn_primario_fondo_ini = '#7267EF',
+        btn_primario_fondo_fin = '#7267EF',
+        btn_primario_fuente = '#ffffff',
+        btn_peligro_fondo_ini = '#ea4d4d',
+        btn_peligro_fondo_fin = '#ea4d4d',
+        btn_peligro_fuente = '#ffffff',
+        btn_accion_fondo_ini = '#f8f9fa',
+        btn_accion_fondo_fin= '#f8f9fa',
+        btn_accion_fuente = '#212529',
+        alerta_exito_fondo = '#d1f4e0',
+        alerta_exito_fuente = '#0e773d',
+        alerta_problema_fondo = '#fbdbdb',
+        alerta_problema_fuente = '#8c2e2e'
+      WHERE id=1;
+    SQL
+  end
+  def down
+    execute <<-SQL
+      UPDATE sip_tema SET
+        nombre='AZUL POR OMISIÓN',
+        fondo = '#ffffff',
+        color_fuente = '#000000',
+        nav_ini = '#95c4ff',
+        nav_fin = '#266dd3',
+        nav_fuente = '#ffffff',
+        fondo_lista = '#e3e1fc',
+        color_flota_subitem_fuente = '#ffffff',
+        color_flota_subitem_fondo = '#266dd3',
+        btn_primario_fondo_ini = '#0088cc',
+        btn_primario_fondo_fin = '#0044cc',
+        btn_primario_fuente = '#ffffff',
+        btn_peligro_fondo_ini = '#ee5f5b',
+        btn_peligro_fondo_fin = '#bd362f',
+        btn_peligro_fuente = '#ffffff',
+        btn_accion_fondo_ini = '#ffffff',
+        btn_accion_fondo_fin= '#e6e6e6',
+        btn_accion_fuente = '#000000',
+        alerta_exito_fondo = '#dff0d8',
+        alerta_exito_fuente = '#468847',
+        alerta_problema_fondo = '#f8d7da',
+        alerta_problema_fuente = '#721c24'
+      WHERE id=1;
+    SQL
+  end
+end
