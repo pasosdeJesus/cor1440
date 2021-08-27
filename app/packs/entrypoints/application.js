c8d07809e715338aba41a6ef0da2c99a1bc05a4c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-08-27 17:24:55 -0500

    usa bootstrap 5

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -33,7 +33,7 @@ window.ApexCharts = ApexCharts
 import $ from "expose-loader?exposes=$,jQuery!jquery";
 
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
-import 'bootstrap'              // Maquetacion y elementos de diseño
+import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
