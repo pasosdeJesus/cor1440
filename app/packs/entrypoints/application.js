d18adc3f4e726b65c085a48cfa761e67dd63eab4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-08-27 17:24:55 -0500

    usa bootstrap 5

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -41,7 +41,7 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import PerfectScrollbar from 'perfect-scrollbar';
 
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
-import 'bootstrap'              // Maquetacion y elementos de diseño
+import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
