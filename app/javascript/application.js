23517966230382609469ae02eecbe2710a09224a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-27 14:53:18 -0500

    Renombrando recursos_globales por recursos_sprockets y afinando arranque javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -26,3 +26,52 @@ Apex.chart = {
 
 import 'gridstack'
 
+import {AutocompletaAjaxExpreg} from '@pasosdejesus/autocompleta_ajax'
+window.AutocompletaAjaxExpreg = AutocompletaAjaxExpreg
+
+
+let esperarRecursosSprocketsYDocumento = function (resolver) {
+  if (typeof window.puntomontaje == 'undefined') {
+    setTimeout(esperarRecursosSprocketsYDocumento, 100, resolver)
+    return false
+  }
+  if (document.readyState !== 'complete') {
+    setTimeout(esperarRecursosSprocketsYDocumento, 100, resolver)
+    return false
+  }
+  resolver("Recursos manejados con sprockets cargados y documento presentado en navegador")
+    return true
+  }
+
+let promesaRecursosSprocketsYDocumento = new Promise((resolver, rechazar) => {
+  esperarRecursosSprocketsYDocumento(resolver)
+})
+
+
+promesaRecursosSprocketsYDocumento.then((mensaje) => {
+  console.log(mensaje)
+  var root = window 
+  root.cor1440_gen_activa_autocompleta_mismotipo = true
+  sip_prepara_eventos_comunes(root);
+  sip_prepara_eventos_comunes(root);
+  heb412_gen_prepara_eventos_comunes(root);
+  mr519_gen_prepara_eventos_comunes(root);
+  cor1440_gen_prepara_eventos_comunes(root);
+
+})
+
+
+document.addEventListener('turbo:load', (e) => {
+ /* Lo que debe ejecutarse cada vez que turbo cargue una página,
+ * tener cuidado porque puede dispararse el evento turbo varias
+ * veces consecutivas al cargarse  la misma página.
+ */
+  
+  console.log('Escuchador turbo:load')
+
+  sip_ejecutarAlCargarPagina(window)
+})
+
+
+
+
