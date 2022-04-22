7a67af15e640a47b4dde5a7f5d60bcb19ec813f2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-04-22 05:27:00 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -1802,6 +1802,8 @@ CREATE TABLE public.sip_departamento (
     id_pais integer DEFAULT 0 NOT NULL,
     id integer DEFAULT nextval('public.sip_departamento_id_seq'::regclass) NOT NULL,
     observaciones character varying(5000) COLLATE public.es_co_utf_8,
+    codiso character varying(6),
+    catiso character varying(64),
     CONSTRAINT departamento_check CHECK (((fechadeshabilitacion IS NULL) OR (fechadeshabilitacion >= fechacreacion)))
 );
 
@@ -6566,6 +6568,8 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220413123127'),
 ('20220417203841'),
 ('20220417220914'),
-('20220417221010');
+('20220417221010'),
+('20220420143020'),
+('20220420154535');
 
 
