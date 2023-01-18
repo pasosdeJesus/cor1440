f6560089a630f2fbea5e261fb6bd6b6d62ff2418
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-17 22:21:01 -0500

    Pasando a msip

--- config/initializers/msip.rb
+++ config/initializers/msip.rb
@@ -12,9 +12,9 @@ Msip.setup do |config|
   config.titulo = "Cor1440 #{Cor1440Gen::VERSION}"
 
   config.descripcion = "Aplicación genérica para proyectos y actividades con metodología de marco lógico"
-  config.codigofuente = "https://github.com/pasosdeJesus/cor1440"
-  config.urlcontribuyentes = "https://github.com/pasosdeJesus/cor1440_gen/graphs/contributors"
-  config.urlcreditos = "https://github.com/pasosdeJesus/cor1440_gen/blob/master/CREDITOS.md"
+  config.codigofuente = "https://gitlab.com/pasosdeJesus/cor1440"
+  config.urlcontribuyentes = "https://gitlab.com/pasosdeJesus/cor1440_gen/-/graphs/main?ref_type=heads"
+  config.urlcreditos = "https://gitlab.com/pasosdeJesus/cor1440_gen/-/blob/main/CREDITOS.md"
   config.agradecimientoDios = "<p>
 Agradecemos a Dios que es Dios de orden
 </p>
